8e8dee4645aac7d31006fe2087eb92aee76b24c0
Abdou Yakubu <yaabdullahi53@gmail.com>
2025-04-02 19:13:28 +0300

    this is a new commit

--- obj/Debug/net9.0/myWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/myWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e0e05099a4abae6c41a94b3ebafe3eaf21edc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+233adf3c0e1d12c830837e74791bac3fae38c935")]
 [assembly: System.Reflection.AssemblyProductAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
