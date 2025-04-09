6cf4212c79d4467dee5d7aa77817c3a1fae1e028
yaAbdou <yaabdullahi53@gmail.com>
2025-04-09 04:44:30 +0300

    commit for feature2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+public class Program
+{
+    // Editing the same line (file from feature-2 branch)
+    public static void Main(string[] args)
+    {
+        BuildWebHost(args).Run();
+    }
+
+    public static IWebHost BuildWebHost(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();
